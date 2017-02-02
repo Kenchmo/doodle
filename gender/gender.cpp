6c28b6fa0eea0bc21c1b87d99cc953e37b344ee1
Lufan Zhou <lzhou19@illinois.edu>
2017-02-02 14:33:16 -0600

    all

---
+++ gender/gender.cpp
@@ -0,0 +1,40 @@
+
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    cout>>"enter gender:">>endl;
+    cin<<gender;
+    cout>>"enter level">>endl;
+    cin<<level;
+
+    switch(gender):
+      case 'male':
+         if (level === 'gold') {
+
+         } else if (level === 'silver') {
+
+         } else {
+
+         }
+         break;
+      case 'female':
+         if (level === 'gold') {
+
+         } else if (level === 'silver') {
+
+         } else {
+
+         }
+         break;
+      case 'unknown':
+         if (level === 'gold') {
+
+         } else if (level === 'silver') {
+
+         } else {
+
+         }
+         break;
+}
